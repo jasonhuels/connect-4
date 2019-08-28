19b82d10d247b21a52de9754fd6c626bd0b6793a
Alan Hermanns, Christine Frank and Jason Huels <christine@>
2019-08-28 14:23:48 -0700

    Added functionality to start 2 player game and change turns

--- js/scripts.js
+++ js/scripts.js
@@ -15,16 +15,19 @@ function Board() {
 }
 
 Board.prototype.initBoard = function() {
-  for(let i=0; i<7; i++) {
+  this.board = [];
+  for(let i=0; i<=6; i++) {
     this.board.push(new Array(7));
   }
+  this.movesLeft = 42;
+  this.turn = Math.round(Math.random());
 }
 
-Board.prototype.updateBoard = function(playerNum, col) {
+Board.prototype.updateBoard = function(col) {
   var output = false;
-  for(let row=0;row<=6; row++){
-    if(!this.board[row][col]){
-      this.board[row][col] = playerNum;
+  for(let row=0;row<=5; row++){
+    if(!(this.board[row][col] === 0 || this.board[row][col] === 1)){
+      this.board[row][col] = this.turn;
       output = true;
       this.movesLeft--;
       break;
@@ -38,8 +41,8 @@ Board.prototype.checkWinVert = function(col){
   var playerNum = -1;
   var count = 0;
   var win = false;
-    for(let row=0; row<=6; row++){
-      if(this.board[row][col]){
+    for(let row=0; row<=5; row++){
+      if(this.board[row][col] === 0 || this.board[row][col] === 1){
         if(this.board[row][col] === playerNum){
           count++;
         }
@@ -60,8 +63,8 @@ Board.prototype.checkWinHorz = function(row){
   var playerNum = -1;
   var count = 0;
   var win = false;
-    for(let col=0; col<=7; col++){
-      if(this.board[row][col]){
+    for(let col=0; col<=6; col++){
+      if(this.board[row][col] === 0 || this.board[row][col] === 1){
         if(this.board[row][col] === playerNum){
           count++;
         }
@@ -89,3 +92,67 @@ Board.prototype.changeTurn = function(){
 }
 
 ////////////// UI /////////////////////
+$(document).ready(function(){
+  var board = new Board();
+  $("#p-v-p").click(function() {
+    makePlayer(board);
+    makePlayer(board);
+    $("#p1-type").text(board.players[0].name);
+    $("#p2-type").text(board.players[1].name);
+
+    $(".col").removeClass("p1");
+    $(".col").removeClass("p2");
+    board.initBoard();
+    $("#play-mode").fadeOut("slow");
+    $("#scores").fadeIn("slow");
+    $("#board").fadeIn("slow");
+
+    showActivePlayer(board);
+  });
+
+  $(".col-choice").click(function(){
+    //var locations = ["zero", "one", "two", "three", "four", "five", "six"];
+    var colNum = parseInt($(this).parent().attr("class").split(" ")[1]);
+    //var colNum = locations.indexOf(colString);
+    board.updateBoard(colNum);
+    colorBoard(board, colNum);
+    board.changeTurn();
+    showActivePlayer(board);
+  });
+});
+
+function colorBoard(board, col){
+  var boardArr = board.board;
+  for(var row=5; row>=0; row--){
+    var entry = boardArr[row][col];
+    if(entry === board.turn){
+      var classToAdd = "p" + (entry+1);
+      $("#"+row+" ." +col).addClass(classToAdd);
+      break;
+    }
+  }
+}
+
+function makePlayer(board){
+  var playerCount = board.players.length + 1;
+  var pName = prompt("Enter Player " + playerCount + " Name");
+  if(!pName){
+    pName = "Player "+ playerCount;
+  }
+  var player = new Player(pName);
+  board.players.push(player);
+}
+
+function showActivePlayer(board) {
+  if(board.turn === 0) {
+    $("#p1").addClass("active");
+    $("#p1").addClass("p1");
+    $("#p2").removeClass("active");
+    $("#p2").removeClass("p2");
+  } else {
+    $("#p2").addClass("active");
+    $("#p2").addClass("p2");
+    $("#p1").removeClass("active");
+    $("#p1").removeClass("p1");
+  }
+}
